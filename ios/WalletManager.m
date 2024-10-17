1e3dd376452eadb4c557c688b513f9faf452811e
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2024-10-17 14:18:10 +0330

    chore: refactor IOS addPassFromUrl

--- ios/WalletManager.m
+++ ios/WalletManager.m
@@ -51,7 +51,7 @@ RCT_EXPORT_METHOD(
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject
                   ) {
-  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+  dispatch_async(dispatch_get_main_queue(), ^{
     NSURL *passURL = [[NSURL alloc] initWithString:pass];
     if (!passURL) {
       reject(rejectCode, @"The pass URL is invalid", nil);
